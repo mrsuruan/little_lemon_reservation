47e7d8217c1266457803a335650b76cce5ff5689
mrsuruan <119714947+mrsuruan@users.noreply.github.com>
2023-05-14 12:09:04 -1000

    Header component Header.js with styles Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,29 @@
+// Header.js
+
+import '../styles/Header.css'
 import logo_small from "../images/logo_small.png";
-import { Box, HStack } from "@chakra-ui/react";
+// import { Box, HStack } from "@chakra-ui/react";
 
 function Header() {
     return (
-        <header>
-            <Box maxWidth="1280px" margin="0 auto">
-                <HStack
-                    px={100}
-                    py={10}
-                    justifyContent="space-between"
-                    alignItems="center"
-                >
-                    <a href="/">
-                        <img src={logo_small} style={{width: "150px"}} alt="Little Lemon logo"/>
-                    </a>
-                    <nav>
-                        <HStack spacing={24}>
-                            <a href="/">Home</a>
-                            <a href="/menu">Menu</a>
-                            <a href="/order">Order</a>
-                            <a href="/about">About</a>
-                            <a href="/login">Login</a>
-                            <a href="/reservations">
-                                <button>Reserve a Table</button>
-                            </a>
-                        </HStack>
-                    </nav>
-                </HStack>
-            </Box>
+        <header className="header" >
+            <nav className="navbar">
+                <a className="logo" href="/" title="Little Lemon Restaurant">
+                    <img src={logo_small} style={{width: "200px"}} title="Little Lemon Restaurant" alt="Little Lemon logo"/>
+                </a>
+                <ul className="nav-links">
+                    <li><a href="/" title="Home page">Home</a></li>
+                    <li><a href="/menu" title="Restaurant menu">Menu</a></li>
+                    <li><a href="/order" title="Order online">Order</a></li>
+                    <li><a href="/about" title="About Little Lemon Restaurant">About</a></li>
+                    <li><a href="/login" title="Login to your account">Login</a></li>
+                </ul>
+                <a href="/reservations">
+                    <button className="reserve-btn" aria-label="Reserve a Table">
+                        Reserve a Table
+                    </button>
+                </a>
+            </nav>
         </header>
     );
 }
