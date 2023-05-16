db3eae99fb507107a7a54c4ed85bb79445066e7c
mrsuruan <119714947+mrsuruan@users.noreply.github.com>
2023-05-15 17:38:52 -1000

    Footer.js and Footer.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+// Footer.js
+
+import "../styles/Footer.css"
 import logo_2 from "../images/logo_2.png";
 import { Box, HStack, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,14 +10,6 @@ import {
     faTiktok,
     faYoutube
     } from "@fortawesome/free-brands-svg-icons";
-
-// function SocialLinks() {
-//     return (
-//         <a href="https:facebook.com/">
-//             <FontAwesomeIcon icon={faFacebook} size="small"/>
-//         </a>
-//     )
-// }
 
 const socials = [
     {
@@ -32,95 +27,112 @@ const socials = [
     {
         icon: faYoutube,
         url: "https://www.youtube.com"
-    }
+    },
 ];
 
-
-function Footer() {
+const Footer = () => {
     return (
-        <footer>
-            <Box maxWidth="1280px" margin="0 auto">
-                <HStack
-                    px={100}
-                    justifyContent="space-between"
-                    alignItems="top"
-                >
-                    <a href="/">
-                        <img src={logo_2} style={{width: "50px"}} alt="Little Lemon footer logo"/>
-                    </a>
-                    <VStack alignItems="left">
-                        <h4>
-                            Sitemap
-                        </h4>
-                            <a href="/">Home</a>
-                            <a href="/menu">Menu</a>
-                            <a href="/order">Order</a>
-                            <a href="/about">About</a>
-                            <a href="/login">Login</a>
-                            <a href="/reservations">Reservations</a>
-                    </VStack>
-                    <VStack alignItems="left">
-                        <h4>
-                            Location
-                        </h4>
-                        <p>
-                            813 W. Randolph St
-                        </p>
-                        <p>
-                            Chicago, IL 60654
-                        </p>
-                        <h4>
-                            Hours
-                        </h4>
-                        <p>
-                            Sunday - Thursday
-                        </p>
-                        <p>
-                            5:00 PM - 10:00 PM
-                        </p>
-                        <p>
-                            Friday - Saturday
-                        </p>
-                        <p>
-                            4:00 PM - 10:00 PM
-                        </p>
-                    </VStack>
-                    <VStack>
-                        <h4>
-                            Contact
-                        </h4>
-                        <p>
-                            <a href="mailto:eat@littlelemon.com">eat@littlelemon.com</a>
-                        </p>
-                        <p>
-                            <a href="tel:3124920808">(312)492-0808</a>
-                        </p>
-                        <h4>
-                            Follow us
-                        </h4>
-                        <HStack>
-                            {socials.map(({ icon, url }) => (
-                                <a
-                                key={url}
-                                href={url}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                >
-                                    <FontAwesomeIcon icon={icon} size="1x" key={url} />
-                                </a>
-                            ))}
-
-                        </HStack>
-                        
-                    </VStack>
-
+        <Box className="footer-background">
+            <Box as="footer" pt ="20" pb="20" className="footer">
+                <HStack px={10} justifyContent="space-between" alignItems="top" >
+                    <Box className="footer-logo">
+                        <a href="/">
+                            <img src={logo_2} style={{width: "50px"}} alt="Little Lemon footer logo"/>
+                        </a>
+                    </Box>
+                    <Box>
+                        <VStack alignItems="left">
+                            <h4 className="footer-header">Sitemap</h4>
+                            <ul className="footer-nav-links">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/reservations">Reservations</a></li>
+                                <li><a href="/menu">Menu</a></li>
+                                <li><a href="/order">Order</a></li>
+                                <li><a href="/about">About</a></li>
+                                <li><a href="/login">Login</a></li>
+                            </ul>
+                        </VStack>
+                    </Box>
+                    <Box>
+                        <VStack align="left">
+                            <Box marginBottom="10px">
+                                <h4 className="footer-header">
+                                    Location
+                                </h4>
+                                <p>
+                                    813 W. Randolph St
+                                </p>
+                                <p>
+                                    Chicago, IL 60654
+                                </p>
+                            </Box>
+                            <Box>
+                                <h4 className="footer-header">
+                                    Hours
+                                </h4>
+                                <p>
+                                    Sunday - Thursday
+                                </p>
+                                <p>
+                                    5:00 PM - 10:00 PM
+                                </p>
+                                <p>
+                                    Friday - Saturday
+                                </p>
+                                <p>
+                                    4:00 PM - 10:00 PM
+                                </p>
+                            </Box>
+                        </VStack>
+                    </Box>
+                    <Box>
+                        <VStack align="left" >
+                            <Box marginBottom="10px">
+                                <h4 className="footer-header">
+                                    Contact
+                                </h4>
+                                <Box className="footer-contact">
+                                    <a href="mailto:eat@littlelemon.com">
+                                        <p>eat@littlelemon.com</p>
+                                    </a>
+                                    <a href="tel:3124920808">
+                                        <p>(312)492-0808</p>
+                                    </a>
+                                </Box>
+                            </Box>
+                            <Box>
+                                <h4 className="footer-header">
+                                    Follow us
+                                </h4>
+                                <HStack spacing="15px">
+                                    {socials.map(({ icon, url }) => (
+                                        <a
+                                        key={url}
+                                        href={url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="footer-social-icons"
+                                        >
+                                            <FontAwesomeIcon icon={icon} size="1.5x" key={url} color="#EDEFEE" />
+                                        </a>
+                                    ))}
+                                </HStack>
+                            </Box>
+                        </VStack>
+                    </Box>
                 </HStack>
-                <p>
-                    © 2023 Little Lemon Restaurant, LLC. All Rights Reserved | Accessibility | Privacy Policy
-                </p>
+                <Box className="footer-copyright" textAlign="center" pt="20px">
+                    <p>
+                        © 2023 Little Lemon Restaurant, LLC. All Rights Reserved
+                        <span class="footer-separator">|</span>
+                        <a href="/accessibility">Accessibility</a>
+                        <span class="footer-separator">|</span>
+                        <a href="privacy-policy">Privacy Policy</a>
+                    </p>
+                </Box>
             </Box>
-        </footer>
+        </Box>
     );
-}
+};
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,31 @@
 
 import '../styles/Header.css'
 import logo_small from "../images/logo_small.png";
-// import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 
-function Header() {
+const Header = () => {
     return (
-        <header className="header" >
-            <nav className="navbar">
-                <a className="logo" href="/" title="Little Lemon Restaurant">
-                    <img src={logo_small} style={{width: "200px"}} title="Little Lemon Restaurant" alt="Little Lemon logo"/>
-                </a>
-                <ul className="nav-links">
-                    <li><a href="/" title="Home page">Home</a></li>
-                    <li><a href="/menu" title="Restaurant menu">Menu</a></li>
-                    <li><a href="/order" title="Order online">Order</a></li>
-                    <li><a href="/about" title="About Little Lemon Restaurant">About</a></li>
-                    <li><a href="/login" title="Login to your account">Login</a></li>
-                </ul>
-                <a href="/reservations">
-                    <button className="reserve-btn" aria-label="Reserve a Table">
-                        Reserve a Table
-                    </button>
-                </a>
-            </nav>
-        </header>
+        <Box className="header-background">
+            <header className="header" >
+                <nav className="navbar">
+                    <a className="logo" href="/" title="Little Lemon Restaurant">
+                        <img src={logo_small} style={{width: "200px"}} title="Little Lemon Restaurant" alt="Little Lemon logo"/>
+                    </a>
+                    <ul className="nav-links">
+                        <li><a href="/" title="Home page">Home</a></li>
+                        <li><a href="/menu" title="Restaurant menu">Menu</a></li>
+                        <li><a href="/order" title="Order online">Order</a></li>
+                        <li><a href="/about" title="About Little Lemon Restaurant">About</a></li>
+                        <li><a href="/login" title="Login to your account">Login</a></li>
+                    </ul>
+                    <a href="/reservations">
+                        <button className="reserve-btn" aria-label="Reserve a Table">
+                            Reserve a Table
+                        </button>
+                    </a>
+                </nav>
+            </header>
+        </Box>
     );
 }
 
